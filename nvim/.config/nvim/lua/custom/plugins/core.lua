e7387d2cce57582fc62087acd3a03a3d14a59a42
William Cho <dev@whjc.me>
2024-03-23 10:10:28 -0300

    shows hidden files when using telescope

--- nvim/.config/nvim/lua/custom/plugins/core.lua
+++ nvim/.config/nvim/lua/custom/plugins/core.lua
@@ -126,16 +126,35 @@ return {
 
       -- [[ Configure Telescope ]]
       -- See `:help telescope` and `:help telescope.setup()`
+      -- Clone the default Telescope configuration
+      local telescopeConfig = require 'telescope.config'
+      local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+      -- search in hidden/dot files.
+      table.insert(vimgrep_arguments, '--hidden')
+      -- search trims indentation on result window
+      table.insert(vimgrep_arguments, '--trim')
+      -- search ignores .git directory
+      table.insert(vimgrep_arguments, '--glob')
+      table.insert(vimgrep_arguments, '!**/.git/*')
+
       require('telescope').setup {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
+        defaults = {
+          --   mappings = {
+          --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+          --   },
+          vimgrep_arguments = vimgrep_arguments,
+        },
+        pickers = {
+          find_files = {
+            -- hidden = true,
+            -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+            find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
