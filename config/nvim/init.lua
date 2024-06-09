fe6b801a30bf7a2ed605c9a3e304876d4428f16e
William Cho <dev@whjc.me>
2024-06-09 12:22:10 -0300

    fix: nvim disable opt.list

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -139,7 +139,7 @@ vim.opt.splitbelow = true
 -- Sets how neovim will display certain whitespace in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
+vim.opt.list = false
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
