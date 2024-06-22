d9eed89a57b02f44b9595f37f2fd41e8b80cce06
William Cho <dev@whjc.me>
2024-06-22 09:29:36 -0300

    fix: remove extra <leader>r rename keymap

--- config/nvim/lua/custom/plugins/core.lua
+++ config/nvim/lua/custom/plugins/core.lua
@@ -66,7 +66,6 @@ return {
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
         ['<leader>d'] = { name = '[D]ocument/[D]ebugger', _ = 'which_key_ignore' },
         ['<leader>f'] = { name = '[F]ile', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>t'] = { name = '[T]est', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
