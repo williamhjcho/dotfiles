359973921b917f6550208ced5c9ec90311e78188
William Cho <dev@whjc.me>
2024-09-25 07:50:19 -0300

    fix: tmux sh detached split window

--- bin/tmux.sh
+++ bin/tmux.sh
@@ -12,22 +12,22 @@ if [ $? != 0 ]; then
   tmux new-window -t $SESH -n "backend"
   tmux send-keys -t $SESH:backend "cd ~/dev/porfin/backend" C-m
   tmux send-keys -t $SESH:backend "nvim ." C-m
-  tmux split-window -t $SESH:backend -h
+  tmux split-window -t $SESH:backend -h -d
 
   tmux new-window -t $SESH -n "functions"
   tmux send-keys -t $SESH:functions "cd ~/dev/porfin/cloud-functions" C-m
   tmux send-keys -t $SESH:functions "nvim ." C-m
-  tmux split-window -t $SESH:functions -h
+  tmux split-window -t $SESH:functions -h -d
 
   tmux new-window -t $SESH -n "iac"
   tmux send-keys -t $SESH:iac "cd ~/dev/porfin/iac" C-m
   tmux send-keys -t $SESH:iac "nvim ." C-m
-  tmux split-window -t $SESH:iac -h
+  tmux split-window -t $SESH:iac -h -d
 
   tmux new-window -t $SESH -n "porfin-ai"
   tmux send-keys -t $SESH:porfin-ai "cd ~/dev/porfin/crew" C-m
   tmux send-keys -t $SESH:porfin-ai "nvim ." C-m
-  tmux split-window -t $SESH:porfin-ai -h
+  tmux split-window -t $SESH:porfin-ai -h -d
 
   tmux select-window -t $SESH:dotfiles
 fi
