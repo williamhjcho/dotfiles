7cb27182e7426e3ca41c45b7c4be321627f420c9
William Cho <dev@whjc.me>
2024-10-15 07:54:56 -0300

    feat: adds avante

---
+++ config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,50 @@
+return {
+  -- https://github.com/yetone/avante.nvim
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      -- Make sure to setup the file types and ft properly
+      "MeanderingProgrammer/render-markdown.nvim",
+    },
+    keys = {},
+    opts = {
+      provider = "claude",
+      auto_suggestions_provider = "claude",
+      behaviour = {
+        auto_suggestions = false,
+      },
+    },
+    config = function(_, opts)
+      require("avante").setup(opts)
+      -- recommended to be executed after color scheme setup
+      require("avante_lib").load()
+    end,
+  },
+}
