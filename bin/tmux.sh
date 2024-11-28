8ca328ded1b91ed749aa656fe55706ccf0b4c76e
William Cho <dev@whjc.me>
2024-11-28 11:24:39 -0300

    fix: tmux session windows

--- bin/tmux.sh
+++ bin/tmux.sh
@@ -11,23 +11,18 @@ if [ $? != 0 ]; then
   tmux send-keys -t $SESH:dotfiles "nvim ." C-m
   tmux split-window -t $SESH:dotfiles -h -d -c "$HOME/dotfiles"
 
-  tmux new-window -t $SESH -n "backend" -c "$PORFIN/backend"
-  tmux split-window -t $SESH:backend -h -d -c "$PORFIN/backend"
+  tmux new-window -t $SESH -n "porfin/backend" -c "$PORFIN/backend"
+  tmux split-window -t "$SESH:porfin/backend" -h -d -c "$PORFIN/backend"
 
-  tmux new-window -t $SESH -n "functions" -c "$PORFIN/cloud-functions"
-  tmux split-window -t $SESH:functions -h -d -c "$PORFIN/cloud-functions"
+  tmux new-window -t $SESH -n "porfin/cloud" -c "$PORFIN/porfin-cloud"
+  tmux split-window -t "$SESH:porfin/cloud" -h -d -c "$PORFIN/porfin-cloud"
 
-  tmux new-window -t $SESH -n "iac" -c "$PORFIN/iac"
-  tmux split-window -t $SESH:iac -h -d -c "$PORFIN/iac"
-
-  tmux new-window -t $SESH -n "porfin-cloud" -c "$PORFIN/porfin-cloud"
-  tmux split-window -t $SESH:porfin-cloud -h -d -c "$PORFIN/porfin-cloud"
-
-  tmux new-window -t $SESH -n "vs" -c "$WHJC/vs"
+  tmux new-window -t $SESH -n "porfin/doctor" -c "$PORFIN/doctor-front"
+  tmux split-window -t "$SESH:porfin/doctor" -h -d -c "$PORFIN/doctor-front"
 
   tmux new-window -t $SESH -n "whjc.ai" -c "$WHJC/whjc.ai"
 
-  tmux select-window -t $SESH:dotfiles
+  tmux select-window -t "$SESH:dotfiles"
 fi
 
 tmux attach-session -t $SESH
