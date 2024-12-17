8c090b56a55d2010b2318d3aab8f8bb695cca8ff
William Cho <dev@whjc.me>
2024-12-17 09:23:08 -0300

    fix: blink completion with super-tab

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -46,6 +46,20 @@ return {
     version = false, -- last release is way too old
   },
   {
+    "saghen/blink.cmp",
+    lazy = false,
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' for mappings similar to built-in completion
+      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+      -- see the "default configuration" section below for full documentation on how to define
+      -- your own keymap.
+      keymap = { preset = "super-tab" },
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
       opts.inlay_hints = { enabled = false }
