9f89d0ac333ac7d447e83ad769768df95734f948
William Cho <dev@whjc.me>
2024-12-25 08:57:10 -0300

    wip

--- config/nvim/lua/plugins/go.lua
+++ config/nvim/lua/plugins/go.lua
@@ -89,6 +89,40 @@ return {
       },
     },
   },
+
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = function(_, opts)
+  --     opts.inlay_hints = { enabled = false }
+  --
+  --     vim.list_extend(opts.servers, {
+  --       -- managed by plugin lazyvim.plugins.extras.lang.go
+  --       gopls = {
+  --         -- https://github.com/golang/tools/blob/master/gopls/doc/settings.md
+  --         settings = {
+  --           gopls = {
+  --             gofumpt = true,
+  --             analyses = {
+  --               fieldalignment = true,
+  --               nilness = true,
+  --               unusedparams = true,
+  --               unusedwrite = true,
+  --               useany = true,
+  --             },
+  --           },
+  --         },
+  --       },
+  --       -- gofumpt = {},
+  --       -- golangci_lint_ls = {},
+  --       templ = {},
+  --     })
+  --     vim.filetype.add({
+  --       extension = {
+  --         templ = "templ",
+  --       },
+  --     })
+  --   end,
+  -- },
   {
     "fredrikaverpil/neotest-golang",
   },
