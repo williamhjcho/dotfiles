0e632a9dc8d0113f557589677dddea3c80116604
William Cho <dev@whjc.me>
2025-01-16 07:34:11 -0300

    chore: remove porfin related windows from tmux

--- bin/tmux.sh
+++ bin/tmux.sh
@@ -11,15 +11,6 @@ if [ $? != 0 ]; then
   tmux send-keys -t $SESH:dotfiles "nvim ." C-m
   tmux split-window -t $SESH:dotfiles -h -d -c "$HOME/dotfiles"
 
-  tmux new-window -t $SESH -n "porfin/backend" -c "$PORFIN/backend"
-  tmux split-window -t "$SESH:porfin/backend" -h -d -c "$PORFIN/backend"
-
-  tmux new-window -t $SESH -n "porfin/cloud" -c "$PORFIN/porfin-cloud"
-  tmux split-window -t "$SESH:porfin/cloud" -h -d -c "$PORFIN/porfin-cloud"
-
-  tmux new-window -t $SESH -n "porfin/doctor" -c "$PORFIN/doctor-front"
-  tmux split-window -t "$SESH:porfin/doctor" -h -d -c "$PORFIN/doctor-front"
-
   tmux new-window -t $SESH -n "whjc.dev" -c "$WHJC/whjc.dev"
 
   tmux select-window -t "$SESH:dotfiles"
