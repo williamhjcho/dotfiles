b522e638b032d2b7be9b482abf35ab3c0e1197db
William Cho <dev@whjc.me>
2025-02-17 12:49:22 -0300

    fix: replace python lua for lazyextra

--- config/nvim/lua/plugins/python.lua
+++ config/nvim/lua/plugins/python.lua
@@ -1,66 +1,67 @@
+-- managed by lazy extras
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = { "python", "ninja", "rst" } },
-  },
-  {
-    "mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "pyright",
-        "basedpyright",
-        "debugpy",
-        "ruff",
-        "ruff-lsp",
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      vim.list_extend(opts.servers, {
-        python = {
-          settings = {
-            pyright = {
-              analysis = {
-                autoSearchPaths = true,
-                useLibraryCodeForTypes = true,
-                diagnosticMode = "openFilesOnly",
-                autoImportCompletions = true,
-              },
-            },
-          },
-        },
-      })
-    end,
-  },
-  {
-    "linux-cultist/venv-selector.nvim",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      "mfussenegger/nvim-dap",
-      "mfussenegger/nvim-dap-python", --optional
-      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
-    },
-    lazy = false,
-    branch = "regexp", -- This is the regexp branch, use this for the new version
-    config = function()
-      require("venv-selector").setup()
-    end,
-    keys = {
-      -- { ",v", "<cmd>VenvSelect<cr>" },
-    },
-  },
-  {
-    "nvim-neotest/neotest",
-    optional = true,
-    dependencies = {
-      "nvim-neotest/neotest-python",
-    },
-    opts = {
-      adapters = {
-        ["neotest-python"] = {},
-      },
-    },
-  },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   opts = { ensure_installed = { "python", "ninja", "rst" } },
+  -- },
+  -- {
+  --   "mason.nvim",
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.ensure_installed, {
+  --       "pyright",
+  --       "basedpyright",
+  --       "debugpy",
+  --       "ruff",
+  --       "ruff-lsp",
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.servers, {
+  --       python = {
+  --         settings = {
+  --           pyright = {
+  --             analysis = {
+  --               autoSearchPaths = true,
+  --               useLibraryCodeForTypes = true,
+  --               diagnosticMode = "openFilesOnly",
+  --               autoImportCompletions = true,
+  --             },
+  --           },
+  --         },
+  --       },
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   "linux-cultist/venv-selector.nvim",
+  --   dependencies = {
+  --     "neovim/nvim-lspconfig",
+  --     "mfussenegger/nvim-dap",
+  --     "mfussenegger/nvim-dap-python", --optional
+  --     { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+  --   },
+  --   lazy = false,
+  --   branch = "regexp", -- This is the regexp branch, use this for the new version
+  --   config = function()
+  --     require("venv-selector").setup()
+  --   end,
+  --   keys = {
+  --     -- { ",v", "<cmd>VenvSelect<cr>" },
+  --   },
+  -- },
+  -- {
+  --   "nvim-neotest/neotest",
+  --   optional = true,
+  --   dependencies = {
+  --     "nvim-neotest/neotest-python",
+  --   },
+  --   opts = {
+  --     adapters = {
+  --       ["neotest-python"] = {},
+  --     },
+  --   },
+  -- },
 }
