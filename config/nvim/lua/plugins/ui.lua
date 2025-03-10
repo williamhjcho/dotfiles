132211c9e1d1076c97f98868fef382f92b273cc7
William Cho <dev@whjc.me>
2025-03-10 10:24:53 -0300

    feat: adds config to nvim dashboard

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -3,6 +3,16 @@ return {
     "folke/snacks.nvim",
     ---@type snacks.Config
     opts = {
+      ---@class snacks.dashboard.Config
+      dashboard = {
+        sections = {
+          { section = "header" },
+          { section = "keys", gap = 1 },
+          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = { 2, 2 } },
+          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 2 },
+          { section = "startup" },
+        },
+      },
       ---@class snacks.scroll.Config
       scroll = {
         animate = {
