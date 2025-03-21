99aca6a63b429b4bff975d3887b7a2e8b22b01bd
William Cho <dev@whjc.me>
2025-03-21 12:58:26 -0300

    fix: golang neotest

--- config/nvim/lua/plugins/go.lua
+++ config/nvim/lua/plugins/go.lua
@@ -17,4 +17,21 @@ return {
       },
     },
   },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      -- "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      { "fredrikaverpil/neotest-golang", version = "*" },
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-golang"),
+        },
+      })
+    end,
+  },
 }
