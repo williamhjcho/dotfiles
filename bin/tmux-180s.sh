cb8d58312b3feff99fe99f2d5f2366175ca8baa4
William Cho <dev@whjc.me>
2025-05-12 10:15:08 -0300

    fix: tmux 180s bin

--- bin/tmux-180s.sh
+++ bin/tmux-180s.sh
@@ -7,23 +7,21 @@ has_session() {
 
 WHJC="whjc"
 if ! has_session "$WHJC"; then
-  PROJECTS_DIR="$HOME/dev/williamhjcho"
   tmux new-session -d -s $WHJC -n "dotfiles" -c "$HOME/dotfiles"
   tmux send-keys -t "$WHJC:dotfiles" "nvim" C-m
-  # tmux split-window -t "$WHJC:dotfiles" -h -d -c "$HOME/dotfiles"
-
-  tmux new-window -t "$WHJC" -n "whjc.dev" -c "$PROJECTS_DIR/whjc.dev"
-  # tmux split-window -t "$WHJC:whjc.dev" -h -d -c "$PROJECTS_DIR/whjc.dev"
-
-  tmux new-window -t "$WHJC" -n "clojure" -c "$PROJECTS_DIR"
-
+  tmux split-window -t "$WHJC:dotfiles" -h -d -c "$HOME/dotfiles"
   tmux select-window -t "$WHJC:dotfiles"
 fi
 
 WORK="180s"
 if ! has_session "$WORK"; then
-  WORK_DIR="$HOME/dev"
+  WORK_DIR="$HOME/dev/180s"
   tmux new-session -d -s "$WORK" -n "180s" -c "$WORK_DIR"
+
+  tmux new-window -t "$WORK" -n "cisne" -c "$WORK_DIR/cisne"
+
+  # tmux new-window -t "$WORK" -n "<project-name>" -c "$WORK_DIR/<dir>"
+  # tmux split-window -t "$WORK:<project-name>" -h -d -c "$WORK_DIR/<dir>"
 fi
 
 SESSION="${1:-$WHJC}"
