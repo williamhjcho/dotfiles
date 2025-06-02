c3b587bf6382e61736fd212392d3175e62d39d61
William Cho <dev@whjc.me>
2025-06-02 10:53:19 -0300

    chore(nvim): move colorschemes to ui.lua

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -1,4 +1,19 @@
 return {
+  -- colorschemes
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      -- storm, moon, night, day
+      style = "moon",
+      on_colors = function(colors)
+        -- makes comments a little brighter so its easier to see
+        colors.comment = "#7c86bf"
+      end,
+    },
+  },
+  -- others
   {
     "folke/snacks.nvim",
     ---@type snacks.Config
