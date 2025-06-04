e49dd413eca081690b0dfc7f3665e5d726e9ac64
William Cho <dev@whjc.me>
2025-06-04 13:12:32 -0300

    fix(nvim): adjust line numbers to be more visible

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -11,6 +11,13 @@ return {
         -- makes comments a little brighter so its easier to see
         colors.comment = "#7c86bf"
       end,
+      on_highlights = function(hl, c)
+        hl.LineNr.fg = c.comment
+        hl.LineNrAbove.fg = c.comment
+        hl.LineNrBelow.fg = c.comment
+        -- hl.CursorLineNr.fg = "#00FF00"
+        -- gl.DiagnosticUnnecessary = { fg = commentColor }
+      end,
     },
   },
   -- others
