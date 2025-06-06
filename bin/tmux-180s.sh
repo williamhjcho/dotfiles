a59b5de8f2dd62c659262b5aadd35c60f1253ce4
William Cho <dev@whjc.me>
2025-06-06 08:56:22 -0300

    fix(tmux): 180s splits

--- bin/tmux-180s.sh
+++ bin/tmux-180s.sh
@@ -11,9 +11,15 @@ if ! has_session "$WHJC"; then
   tmux new-session -d -s $WHJC -n "dotfiles" -c "$HOME/dotfiles"
   tmux send-keys -t "$WHJC:dotfiles" "nvim" C-m
   tmux split-window -t "$WHJC:dotfiles" -h -d -c "$HOME/dotfiles"
+  tmux resize-pane -t "$WHJC:dotfiles.1" -R 60
 
-  tmux new-window -d -t "$WHJC:" -n "vs" -c "$PROJECTS_DIR/vs"
+  tmux new-window -d -t "$WHJC" -n "vs" -c "$PROJECTS_DIR/vs"
   tmux split-window -t "$WHJC:vs" -h -d -c "$PROJECTS_DIR/vs"
+  tmux resize-pane -t "$WHJC:vs.1" -R 60
+
+  tmux new-window -d -t "$WHJC" -n "goodnewz" -c "$PROJECTS_DIR/goodnewz.ai"
+  tmux split-window -t "$WHJC:goodnewz" -h -d -c "$PROJECTS_DIR/goodnewz.ai"
+  tmux resize-pane -t "$WHJC:goodnewz.1" -R 60
 
   tmux select-window -t "$WHJC:dotfiles"
 fi
@@ -24,7 +30,12 @@ if ! has_session "$WORK"; then
   tmux new-session -d -s "$WORK" -n "180s" -c "$WORK_DIR"
 
   tmux new-window -d -t "$WORK:" -n "cisne" -c "$WORK_DIR/cisne"
+  tmux split-window -t "$WORK:cisne" -h -d -c "$WORK_DIR/cisne"
+  tmux resize-pane -t "$WORK:cisne.1" -R 60
+
   tmux new-window -d -t "$WORK:" -n "camaleao" -c "$WORK_DIR/camaleao"
+  tmux split-window -t "$WORK:camaleao" -h -d -c "$WORK_DIR/arraia"
+
   tmux new-window -d -t "$WORK:" -n "tatu" -c "$WORK_DIR/tatu"
 
   # needs the trailing ':' on the session's name
