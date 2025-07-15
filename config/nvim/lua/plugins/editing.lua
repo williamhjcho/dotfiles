54d92be830f9c5fbe195a9607f161b78aa909c3b
William Cho <dev@whjc.me>
2025-07-15 17:11:49 -0300

    feat(nvim): adds hardtime nvim (disabled)

--- config/nvim/lua/plugins/editing.lua
+++ config/nvim/lua/plugins/editing.lua
@@ -3,4 +3,11 @@ return {
     "mg979/vim-visual-multi",
     lazy = false,
   },
+  {
+    "m4xshen/hardtime.nvim",
+    enabled = false,
+    lazy = false,
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {},
+  },
 }
