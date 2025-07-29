b2712dd57a15c5edb9ddc597a5b515a814fb139a
William Cho <dev@whjc.me>
2025-07-29 08:33:39 -0300

    chore(nvim): move linter and formatter to lsp

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -96,4 +96,102 @@ return {
     opts = {},
     event = 'VeryLazy',
   },
+  {
+    'stevearc/conform.nvim',
+    dependencies = { 'mason-org/mason.nvim' },
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    --- @module 'conform'
+    --- @type conform.setupOpts
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          return nil
+        else
+          return {
+            timeout_ms = 500,
+            lsp_format = 'fallback',
+          }
+        end
+      end,
+      formatters_by_ft = {
+        -- general
+        lua = { 'stylua' },
+        sh = { 'beautysh' },
+        zsh = { 'beautysh' },
+        terraform_fmt = { 'terraform' },
+        toml = { 'taplo' },
+        json = { 'biome' },
+        jsonc = { 'biome' },
+        -- terraform
+        hcl = { 'packer_fmt' },
+        terraform = { 'terraform_fmt' },
+        tf = { 'terraform_fmt' },
+        ['terraform-vars'] = { 'terraform_fmt' },
+        -- go
+        go = { 'goimports', 'gofumpt' },
+        templ = { 'templ' }, -- go templ templates
+        -- web/js/ts
+        javascript = { 'biome' },
+        javascriptreact = { 'biome' },
+        typescript = { 'biome' },
+        typescriptreact = { 'biome' },
+        svelte = { 'biome', lsp_format = 'first' },
+        -- dart/flutter
+        dart = { 'dart_format' },
+        -- python
+        python = { 'ruff' },
+        -- clojure
+        clojure = { lsp_format = 'fallback' },
+      },
+    },
+    -- stylua: ignore
+    keys = {
+      { '<leader>cf', function() require('conform').format() end, desc = 'Format Buffer' },
+    },
+  },
+  {
+    'mfussenegger/nvim-lint',
+    opts = {
+      -- Event to trigger linters
+      events = { 'BufWritePost', 'BufReadPost', 'InsertLeave' },
+      linters_by_ft = {
+        --   sh = { 'shellcheck' },
+        --   zsh = { 'zsh' },
+        dockerfile = { 'hadolint' },
+        typescript = { 'biomejs' },
+        python = { 'ruff' },
+      },
+    },
+    config = function(_, opts)
+      local lint = require('lint')
+      lint.linters_by_ft = opts.linters_by_ft
+
+      local M = {}
+      function M.debounce(ms, fn)
+        local timer = vim.uv.new_timer()
+        return function(...)
+          local argv = { ... }
+          timer:start(ms, 0, function()
+            timer:stop()
+            vim.schedule_wrap(fn)(unpack(argv))
+          end)
+        end
+      end
+
+      function M.lint()
+        lint.try_lint()
+      end
+
+      vim.api.nvim_create_autocmd(opts.events, {
+        group = vim.api.nvim_create_augroup('whjc-nvim-lint', { clear = true }),
+        callback = M.debounce(100, M.lint),
+      })
+    end,
+  },
 }
