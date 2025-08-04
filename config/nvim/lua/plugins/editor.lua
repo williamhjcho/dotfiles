4bae4a7150e2d0ce61c660a1be45259559c61c32
William Cho <dev@whjc.me>
2025-08-04 08:52:07 -0300

    fix(nvim): treesitter version latest

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -321,6 +321,7 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    version = false,
     -- Sets main module to use for opts
     main = 'nvim-treesitter.configs',
     opts = {
