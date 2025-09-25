e6d28410e15b4a0aef40dc04d6232ff09dea8834
William Cho <dev@whjc.me>
2025-09-25 10:02:25 -0300

    chore(nvim): add better error msg when lazy clone fails

--- config/nvim/lua/lazy-init.lua
+++ config/nvim/lua/lazy-init.lua
@@ -3,14 +3,20 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
   local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
+    vim.api.nvim_echo({
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
   end
 end
-
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
   spec = {
+    -- import/override with custom plugins
     { import = 'plugins' },
   },
   install = {
