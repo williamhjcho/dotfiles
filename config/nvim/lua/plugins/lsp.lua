8034f7a7f431ef4e4e010d508d71cb10f1820349
William Cho <dev@whjc.me>
2025-10-08 09:58:06 -0300

    chore(nvim): move flutter tools to lsp lua

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -195,4 +195,49 @@ return {
       })
     end,
   },
+
+  {
+    'nvim-flutter/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim',
+    },
+    opts = {
+      closing_tags = { enabled = false },
+      decorations = {
+        statusline = {
+          project_config = true,
+          device = true,
+        },
+      },
+    },
+    keys = {
+      {
+        '<leader>Fl',
+        '<cmd>Telescope flutter commands<cr>',
+        desc = 'Flutter Commands',
+      },
+      {
+        '<leader>Fr',
+        '<cmd>FlutterRun<cr>',
+        desc = 'Flutter Run',
+      },
+      {
+        '<leader>Fq',
+        '<cmd>FlutterQuit<cr>',
+        desc = 'Flutter Quit',
+      },
+      {
+        '<leader>Fdc',
+        '<cmd>FlutterCopyProfilesUrl<cr>',
+        desc = 'Flutter DevTools Copy URL',
+      },
+      -- {
+      --   '<leader>co',
+      --   LazyVim.lsp.action['source.organizeImports'],
+      --   desc = 'Organize Imports',
+      -- },
+    },
+  },
 }
