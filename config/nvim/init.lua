f43640294371124190f66a9ed90bce8a4580082b
William Cho <dev@whjc.me>
2025-11-21 07:18:07 -0300

    chore(nvim): move multicursor to separate file

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -48,6 +48,7 @@ require('whjc.lsp')
 
 -- plugin setups
 require('whjc.mason')
+require('whjc.multicursor')
 require('guess-indent').setup({})
 require('nvim-ts-autotag').setup({})
 require('snacks').setup({
@@ -79,26 +80,4 @@ require('origami').setup({
 })
 require('persistence').setup({})
 
-local multicursor = require('multicursor-nvim')
-multicursor.setup()
--- Mappings defined in a keymap layer only apply when there are
--- multiple cursors. This lets you have overlapping mappings.
-multicursor.addKeymapLayer(function(layerSet)
-  -- Select a different cursor as the main one.
-  layerSet({ 'n', 'x' }, '<left>', multicursor.prevCursor)
-  layerSet({ 'n', 'x' }, '<right>', multicursor.nextCursor)
-
-  -- Delete the current cursor.
-  layerSet({ 'n', 'x' }, '<leader>x', multicursor.deleteCursor)
-
-  -- Enable and clear cursors using escape.
-  layerSet('n', '<esc>', function()
-    if not multicursor.cursorsEnabled() then
-      multicursor.enableCursors()
-    else
-      multicursor.clearCursors()
-    end
-  end)
-end)
-
-require('whjc.lazy')
+-- require('whjc.lazy')
