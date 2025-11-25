edbd51c07b7b4b32602b3d91690f5a45b3bff694
William Cho <dev@whjc.me>
2025-11-25 06:35:42 -0300

    fix(nvim): flutter cmds

--- config/nvim/lua/whjc/flutter.lua
+++ config/nvim/lua/whjc/flutter.lua
@@ -17,10 +17,12 @@ require('flutter-tools').setup({
 })
 
 -- stylua: ignore start
-vim.keymap.set('n', '<leader>Fr', '<cmd>FlutterRun<cr>', { desc = 'Flutter Run', })
-vim.keymap.set('n', '<leader>Fq', '<cmd>FlutterQuit<cr>', { desc = 'Flutter Quit', })
-vim.keymap.set('n', '<leader>FR', '<cmd>FlutterRestart<cr>', { desc = 'Flutter Restart', })
-vim.keymap.set('n', '<leader>Fq', '<cmd>FlutterQuit<cr>', { desc = 'Flutter Quit', })
-vim.keymap.set('n', '<leader>Fl', '<cmd>FlutterLogToggle<cr>', { desc = 'Flutter Log Toggle', })
-vim.keymap.set('n', '<leader>Fdc', '<cmd>FlutterCopyProfilesUrl<cr>', { desc = 'Flutter DevTools Copy URL', })
+vim.keymap.set('n', '<leader>Fr', '<cmd>FlutterRun<cr>', { desc = 'Flutter Run' })
+vim.keymap.set('n', '<leader>Fq', '<cmd>FlutterQuit<cr>', { desc = 'Flutter Quit' })
+vim.keymap.set('n', '<leader>FR', '<cmd>FlutterRestart<cr>', { desc = 'Flutter Restart' })
+vim.keymap.set('n', '<leader>Fq', '<cmd>FlutterQuit<cr>', { desc = 'Flutter Quit' })
+vim.keymap.set('n', '<leader>Fll', '<cmd>FlutterLogToggle<cr>', { desc = 'Flutter Log Toggle' })
+vim.keymap.set('n', '<leader>Flx', '<cmd>FlutterLogClear<cr>', { desc = 'Flutter Log Clear' })
+vim.keymap.set('n', '<leader>Fdo', '<cmd>FlutterOpenDevTools<cr>', { desc = 'Flutter DevTools Open' })
+vim.keymap.set('n', '<leader>Fdy', '<cmd>FlutterCopyProfilesUrl<cr>', { desc = 'Flutter DevTools Copy URL' })
 -- stylua: ignore end
