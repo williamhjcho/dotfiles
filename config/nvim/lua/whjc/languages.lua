ff269b9cf0bb85349921776c0d6d6635e96ae29a
William Cho <dev@whjc.me>
2026-03-03 08:28:10 -0300

    fix(nvim): remove vtsls in favor of tsgo

--- config/nvim/lua/whjc/languages.lua
+++ config/nvim/lua/whjc/languages.lua
@@ -56,8 +56,8 @@ return {
   },
   -- javascript/typescript & web general
   {
-    mason = { 'tsgo', 'vtsls', 'svelte-language-server' },
-    lsp = { 'tsgo', 'vtsls', 'tailwindcss', 'biome' },
+    mason = { 'tsgo', 'svelte-language-server' },
+    lsp = { 'tsgo', 'tailwindcss', 'biome' },
     treesitter = { 'html', 'css', 'jsdoc', 'tsx', 'typescript', 'astro', 'svelte' },
     formatters = {
       css = { 'biome' },
