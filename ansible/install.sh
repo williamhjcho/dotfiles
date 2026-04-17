d04c4dfff8974b9225e5ced835274c83c03846ea
William Cho <dev@whjc.me>
2026-04-17 17:28:06 -0300

    fix(ansible): missing install tags

--- ansible/install.sh
+++ ansible/install.sh
@@ -3,32 +3,32 @@
 set -e
 
 function have {
-  command -v "$1" &>/dev/null
+    command -v "$1" &>/dev/null
 }
 
 have ansible || {
-  echo "Ansible not installed"
-  exit 1
+    echo "Ansible not installed"
+    exit 1
 }
 
 TAGS=""
 while [[ $# -gt 0 ]]; do
-  case "$1" in
-  --dotfiles | --zsh | --homebrew | --dock)
-    TAG="${1#--}"
-    TAGS="${TAGS:+$TAGS,}$TAG"
-    ;;
-  *)
-    echo "Unknown option: '$1'"
-    exit 1
-    ;;
-  esac
-  shift
+    case "$1" in
+        --dotfiles | --zsh | --homebrew | --dock | --macos | --tmux)
+            TAG="${1#--}"
+            TAGS="${TAGS:+$TAGS,}$TAG"
+            ;;
+        *)
+            echo "Unknown option: '$1'"
+            exit 1
+            ;;
+    esac
+    shift
 done
 
 echo "running ansible: ${TAGS:+(tags: $TAGS)}"
 
 ansible-playbook ./macos.yaml \
-  --inventory ./inventory \
-  --ask-become-pass \
-  ${TAGS:+--tags "$TAGS"}
+    --inventory ./inventory \
+    --ask-become-pass \
+    ${TAGS:+--tags "$TAGS"}
