edbd69974309dea244f71921cd8ef25c4210b0da
William Cho <dev@whjc.me>
2026-05-11 10:34:23 -0300

    chore(tmux): update work tmux

--- bin/tmux-180s.sh
+++ bin/tmux-180s.sh
@@ -5,30 +5,18 @@ has_session() {
     return $?
 }
 
-WHJC="whjc"
-if ! has_session "$WHJC"; then
-    PROJECTS_DIR="$HOME/dev/williamhjcho"
-    tmux new-session -d -s $WHJC -n "dotfiles" -c "$HOME/dotfiles"
-    tmux split-window -t "$WHJC:dotfiles" -h -d -c "$HOME/dotfiles"
-    tmux split-window -t "$WHJC:dotfiles.1" -v -d -c "$HOME/dotfiles"
-    tmux resize-pane -t "$WHJC:dotfiles.1" -L 100
-    tmux send-keys -t "$WHJC:dotfiles.3" "nvim" C-m
-
-    tmux new-window -d -t "$WHJC:" -n "memories" -c "$PROJECTS_DIR/memories"
-    tmux split-window -t "$PROJECTS_DIR:memories" -h -d -c "$PROJECTS_DIR/memories"
-    tmux split-window -t "$PROJECTS_DIR:memories.1" -v -d -c "$PROJECTS_DIR/memories"
-    tmux resize-pane -t "$PROJECTS_DIR:memories.1" -L 100
-    tmux select-pane -t "$PROJECTS_DIR:memories.3"
-
-    tmux new-window -d -t "$WHJC:" -n "others" -c "$PROJECTS_DIR"
-
-    tmux select-pane -t "$WHJC:dotfiles.3"
-fi
-
 WORK="180s"
 if ! has_session "$WORK"; then
+    PROJECTS_DIR="$HOME/dev/williamhjcho"
+    tmux new-session -d -s $WORK -n "dotfiles" -c "$HOME/dotfiles"
+    tmux split-window -t "$WORK:dotfiles" -h -d -c "$HOME/dotfiles"
+    tmux split-window -t "$WORK:dotfiles.1" -v -d -c "$HOME/dotfiles"
+    tmux resize-pane -t "$WORK:dotfiles.1" -L 100
+    tmux send-keys -t "$WORK:dotfiles.3" "nvim" C-m
+    tmux select-pane -t "$WORK:dotfiles.3"
+
     WORK_DIR="$HOME/dev/180s"
-    tmux new-session -d -s "$WORK" -n "180s" -c "$WORK_DIR"
+    tmux new-window -d -s "$WORK" -n "180s" -c "$WORK_DIR"
 
     tmux new-window -d -t "$WORK:" -n "cisne" -c "$WORK_DIR/cisne"
     tmux split-window -t "$WORK:cisne" -h -d -c "$WORK_DIR/cisne"
@@ -36,15 +24,17 @@ if ! has_session "$WORK"; then
     tmux resize-pane -t "$WORK:cisne.1" -L 100
     tmux select-pane -t "$WORK:cisne.3"
 
-    tmux new-window -d -t "$WORK:" -n "camaleao" -c "$WORK_DIR/camaleao"
-    tmux new-window -d -t "$WORK:" -n "tatu" -c "$WORK_DIR/tatu"
+    tmux new-window -d -t "$WORK:" -n "channels" -c "$WORK_DIR/lobo"
+    tmux split-window -t "$WORK:channels" -h -d -c "$WORK_DIR/camaleao"
+
+    tmux new-window -d -t "$WORK:" -n "others" -c "$WORK_DIR"
 
     # needs the trailing ':' on the session's name
     # tmux new-window -t "$WORK:" -n "<project-name>" -c "$WORK_DIR/<dir>"
     # tmux split-window -t "$WORK:<project-name>" -h -d -c "$WORK_DIR/<dir>"
 fi
 
-SESSION="${1:-$WHJC}"
+SESSION="${1:-$WORK}"
 if has_session "$SESSION"; then
     tmux attach-session -t "$SESSION"
 else
