e01d5163f89873dbe31d3a9a724937d92c7c1381
William Cho <dev@whjc.me>
2026-07-02 07:34:06 -0300

    chore(nvim): update flutter config

--- config/nvim/lua/whjc/flutter.lua
+++ config/nvim/lua/whjc/flutter.lua
@@ -1,39 +1,36 @@
-local later = Config.later
 local add = vim.pack.add
 
-later(function()
-  add({
-    'https://github.com/nvim-flutter/flutter-tools.nvim',
+add({
+  'https://github.com/nvim-flutter/flutter-tools.nvim',
 
-    -- dependencies
-    'https://github.com/nvim-lua/plenary.nvim',
-    'https://github.com/stevearc/dressing.nvim',
-  }, { confirm = false })
+  -- dependencies
+  'https://github.com/nvim-lua/plenary.nvim',
+  'https://github.com/stevearc/dressing.nvim',
+})
 
-  require('flutter-tools').setup({
-    closing_tags = { enabled = false },
-    decorations = {
-      statusline = {
-        project_config = true,
-        device = true,
-      },
+require('flutter-tools').setup({
+  closing_tags = { enabled = false },
+  decorations = {
+    statusline = {
+      project_config = true,
+      device = true,
     },
-    debugger = {
-      enabled = true,
-    },
-    dev_tools = {
-      autostart = true,
-      auto_open_browser = false,
-    },
-  })
+  },
+  debugger = {
+    enabled = true,
+  },
+  dev_tools = {
+    autostart = true,
+    auto_open_browser = false,
+  },
+})
 
-  vim.keymap.set('n', '<leader>Fr', '<cmd>FlutterRun<cr>', { desc = 'Flutter Run' })
-  vim.keymap.set('n', '<leader>Fd', '<cmd>FlutterDevices<cr>', { desc = 'Flutter Devices' })
-  vim.keymap.set('n', '<leader>Fq', '<cmd>FlutterQuit<cr>', { desc = 'Flutter Quit' })
-  vim.keymap.set('n', '<leader>FR', '<cmd>FlutterRestart<cr>', { desc = 'Flutter Restart' })
-  vim.keymap.set('n', '<leader>Fq', '<cmd>FlutterQuit<cr>', { desc = 'Flutter Quit' })
-  vim.keymap.set('n', '<leader>Fll', '<cmd>FlutterLogToggle<cr>', { desc = 'Flutter Log Toggle' })
-  vim.keymap.set('n', '<leader>Flx', '<cmd>FlutterLogClear<cr>', { desc = 'Flutter Log Clear' })
-  vim.keymap.set('n', '<leader>Fo', '<cmd>FlutterOpenDevTools<cr>', { desc = 'Flutter DevTools Open' })
-  vim.keymap.set('n', '<leader>Fy', '<cmd>FlutterCopyProfilesUrl<cr>', { desc = 'Flutter DevTools Copy URL' })
-end)
+vim.keymap.set('n', '<leader>Fr', '<cmd>FlutterRun<cr>', { desc = 'Flutter Run' })
+vim.keymap.set('n', '<leader>Fd', '<cmd>FlutterDevices<cr>', { desc = 'Flutter Devices' })
+vim.keymap.set('n', '<leader>Fq', '<cmd>FlutterQuit<cr>', { desc = 'Flutter Quit' })
+vim.keymap.set('n', '<leader>FR', '<cmd>FlutterRestart<cr>', { desc = 'Flutter Restart' })
+vim.keymap.set('n', '<leader>Fq', '<cmd>FlutterQuit<cr>', { desc = 'Flutter Quit' })
+vim.keymap.set('n', '<leader>Fll', '<cmd>FlutterLogToggle<cr>', { desc = 'Flutter Log Toggle' })
+vim.keymap.set('n', '<leader>Flx', '<cmd>FlutterLogClear<cr>', { desc = 'Flutter Log Clear' })
+vim.keymap.set('n', '<leader>Fo', '<cmd>FlutterOpenDevTools<cr>', { desc = 'Flutter DevTools Open' })
+vim.keymap.set('n', '<leader>Fy', '<cmd>FlutterCopyProfilesUrl<cr>', { desc = 'Flutter DevTools Copy URL' })
